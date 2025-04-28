9a68bbd4b1fb7c39587e2c59d6ce82d11bfaa3b9
specta01 <superspecta@gmail.com>
2025-04-27 23:58:53 -0400

    Revert "Reapply "Many Changes""
    This reverts commit ad6fe231158be183d3d6caa39685beb3924a222d.

--- Skelegun/Assets/Shotgun.cs
+++
@@ -1,9 +0,0 @@
-using UnityEngine;
-
-public class Shotgun : Weapon
-{
-    public override void Shoot()
-    {
-        Instantiate(bullet, player.bulletSpawnPoint.position, player.bulletSpawnPoint.rotation);
-    }
-}
