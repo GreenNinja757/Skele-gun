b83829d65d59c9ca1fb262259e6f063ba7b04037
DaleB <154273625+GreenNinja757@users.noreply.github.com>
2025-12-03 04:59:28 -0500

    ProGen
    Ripping my hair out trying to get things working how I want them.

---
+++ Skelegun/Assets/Scripts/Room Generation/RawScripts/Map/MapPlayerIcon.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+
+public class MapPlayerIcon
+{
+    public RectTransform icon;
+    public Transform player;
+    public float scale = 0.1f; // world → minimap scale
+
+
+    void Update()
+    {
+        icon.anchoredPosition = new Vector2(
+            player.position.x * scale,
+            player.position.y * scale
+        );
+    }
+
+
+}
